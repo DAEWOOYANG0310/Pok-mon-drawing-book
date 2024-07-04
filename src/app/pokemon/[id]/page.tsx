46209026634c6a9abae0042d929cf9ddb66046b9
DAEWOOYANG0310 <ydw8088@naver.com>
2024-07-04 15:51:14 +0900

    fix:에러해결

--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -43,7 +43,7 @@ const DetailPage = async ({ params }: { params: { id: string } }) => {
         <div className="mb-6">
           <h3 className="text-xl font-semibold mb-2">기술</h3>
           <div className="grid grid-cols-2 gap-2">
-            {pokemon.moves.slice(0, 20).map((move) => (
+            {pokemon.moves.slice(0, 20).map((move: any) => (
               <div
                 key={move.move.name}
                 className="bg-blue-100 text-blue-800 text-sm font-medium px-2 py-1 rounded"
